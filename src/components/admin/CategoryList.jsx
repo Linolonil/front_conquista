6807a45628878da6b842c479f1eaa6cfec25bdb3
Linolonil <linox999@gmail.com>
2023-11-14 23:38:47 -0400

    adicionando o botao pra editar

--- src/components/admin/CategoryList.jsx
+++ src/components/admin/CategoryList.jsx
@@ -5,9 +5,15 @@ import axios from "axios";
 
 function CategoryList({ title, menuItems, setMenuItems, list }) {
   const [open, setOpen] = useState(false);
+  const [editedItem, setEditedItem] = useState({
+    id: null,
+    name: "",
+    description: "",
+    price: 0,
+  });
 
+  const editItemEndpoint = process.env.NEXT_PUBLIC_PUT_EDITAR;
   const visibilidadeItem = process.env.NEXT_PUBLIC_PUT_ITEM_VISIBILIDADE;
-
   const deleteItem = process.env.NEXT_PUBLIC_DELETE_ITEM;
 
   const filterItemsByCategory = (category) => {
@@ -55,6 +61,53 @@ function CategoryList({ title, menuItems, setMenuItems, list }) {
       });
   };
 
+  const handleEditClick = (item) => {
+    setEditedItem({
+      id: item.id,
+      name: item.name,
+      description: item.description,
+      price: item.price,
+    });
+  };
+
+  const handleSaveClick = () => {
+    // Converta a string para um número usando parseFloat
+    const numericPrice = parseFloat(editedItem.price);
+
+    axios
+      .put(`${editItemEndpoint}/${editedItem.id}`, {
+        ...editedItem,
+        price: numericPrice,
+      })
+      .then((response) => {
+        const updatedItems = menuItems.map((item) => {
+          if (item.id === editedItem.id) {
+            return {
+              ...item,
+              name: editedItem.name,
+              description: editedItem.description,
+              price: numericPrice, // Use o valor convertido
+            };
+          }
+          return item;
+        });
+        setMenuItems(updatedItems);
+        toast.success("Item editado com sucesso");
+        setEditedItem({ id: null, name: "", description: "", price: 0 });
+      })
+      .catch((error) => {
+        toast.error("Erro ao editar o item");
+        console.error("Erro ao editar o item:", error);
+      });
+  };
+
+  const handleInputChange = (e) => {
+    setEditedItem({
+      ...editedItem,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="col-md-6">
       <div className="border rounded p-3 shadow m-2">
@@ -72,34 +125,99 @@ function CategoryList({ title, menuItems, setMenuItems, list }) {
                     : "list-group-item-danger"
                 }`}
               >
-                <span>
-                  <strong className="fs-5" style={{ fontWeight: "bold" }}>
-                    {item.name}
-                  </strong>
-                </span>
-                <br />
-                <span>
-                  <strong>Preço: </strong>R${item.price}
-                </span>
-                <br />
-                <span>
-                  <strong>Ingredientes: </strong>: {item.description}
-                </span>
-                {item.isVisible ? " (Visível)" : " (Oculto)"}
-                <div className="d-flex justify-content-around">
-                  <button
-                    className="btn btn-danger btn-sm ml-2 m-1"
-                    onClick={() => removeItem(item.id)}
-                  >
-                    Excluir
-                  </button>
-                  <button
-                    className="btn btn-secondary btn-sm ml-2 m-1 "
-                    onClick={() => toggleVisibility(item.id, item.isVisible)}
-                  >
-                    Alternar Visibilidade
-                  </button>
-                </div>
+                {editedItem.id === item.id ? (
+                  // Modo de edição
+                  <div>
+                    <div className="mb-3">
+                      <label htmlFor="name" className="form-label">
+                        Nome
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        name="name"
+                        value={editedItem.name}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+
+                    <div className="mb-3">
+                      <label htmlFor="description" className="form-label">
+                        Descrição
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="description"
+                        name="description"
+                        value={editedItem.description}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+
+                    <div className="mb-3">
+                      <label htmlFor="price" className="form-label">
+                        Preço
+                      </label>
+                      <input
+                        type="number"
+                        className="form-control"
+                        id="price"
+                        name="price"
+                        value={editedItem.price}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+
+                    <button
+                      className="btn btn-primary"
+                      onClick={handleSaveClick}
+                    >
+                      Salvar
+                    </button>
+                  </div>
+                ) : (
+                  // Modo de visualização
+                  <div>
+                    <span>
+                      <strong className="fs-5" style={{ fontWeight: "bold" }}>
+                        {item.name}
+                      </strong>
+                    </span>
+                    <br />
+                    <span>
+                      <strong>Preço: </strong>R${item.price}
+                    </span>
+                    <br />
+                    <span>
+                      <strong>Ingredientes: </strong>: {item.description}
+                    </span>
+                    {item.isVisible ? " (Visível)" : " (Oculto)"}
+                    <div className="d-flex justify-content-around">
+                      <button
+                        className="btn btn-danger btn-sm ml-2 m-1"
+                        onClick={() => removeItem(item.id)}
+                      >
+                        Excluir
+                      </button>
+                      <button
+                        className="btn btn-secondary btn-sm ml-2 m-1 "
+                        onClick={() =>
+                          toggleVisibility(item.id, item.isVisible)
+                        }
+                      >
+                        Alternar Visibilidade
+                      </button>
+                      <button
+                        className="btn btn-primary btn-sm ml-2 m-1"
+                        onClick={() => handleEditClick(item)}
+                      >
+                        Editar
+                      </button>
+                    </div>
+                  </div>
+                )}
               </li>
             ))}
           </ul>
--- src/components/admin/SearchItem.jsx
+++ src/components/admin/SearchItem.jsx
@@ -4,7 +4,14 @@ import axios from "axios";
 
 function SearchItem({ menuItems, setMenuItems }) {
   const [searchText, setSearchText] = useState("");
+  const [editedItem, setEditedItem] = useState({
+    id: null,
+    name: "",
+    description: "",
+    price: 0,
+  });
   const visibilidadeItem = process.env.NEXT_PUBLIC_PUT_ITEM_VISIBILIDADE;
+  const editItemEndpoint = process.env.NEXT_PUBLIC_PUT_EDITAR;
 
   const deleteItem = process.env.NEXT_PUBLIC_DELETE_ITEM;
 
@@ -53,6 +60,53 @@ function SearchItem({ menuItems, setMenuItems }) {
     item.name.toLowerCase().includes(searchText.toLowerCase())
   );
 
+  const handleEditClick = (item) => {
+    setEditedItem({
+      id: item.id,
+      name: item.name,
+      description: item.description,
+      price: item.price,
+    });
+  };
+
+  const handleSaveClick = () => {
+    // Converta a string para um número usando parseFloat
+    const numericPrice = parseFloat(editedItem.price);
+
+    axios
+      .put(`${editItemEndpoint}/${editedItem.id}`, {
+        ...editedItem,
+        price: numericPrice,
+      })
+      .then((response) => {
+        const updatedItems = menuItems.map((item) => {
+          if (item.id === editedItem.id) {
+            return {
+              ...item,
+              name: editedItem.name,
+              description: editedItem.description,
+              price: numericPrice, // Use o valor convertido
+            };
+          }
+          return item;
+        });
+        setMenuItems(updatedItems);
+        toast.success("Item editado com sucesso");
+        setEditedItem({ id: null, name: "", description: "", price: 0 });
+      })
+      .catch((error) => {
+        toast.error("Erro ao editar o item");
+        console.error("Erro ao editar o item:", error);
+      });
+  };
+
+  const handleInputChange = (e) => {
+    setEditedItem({
+      ...editedItem,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="">
       <div className="input-group mb-3 border rounded p-3 shadow m-2">
@@ -76,35 +130,96 @@ function SearchItem({ menuItems, setMenuItems }) {
                   : "list-group-item-danger"
               }`}
             >
-              <span>
-                <strong className="fs-5" style={{ fontWeight: "bold" }}>
-                  {item.name}
-                </strong>
-              </span>
-              <br />
-              <span>
-                <strong>Preço: </strong>R${item.price}
-              </span>
-              <br />
-              <span>
-                <strong>Ingredientes: </strong>: {item.description}
-              </span>
-              <br />
-              {item.isVisible ? " (Visível)" : " (Oculto)"}
-              <div className="d-flex justify-content-around">
-                <button
-                  className="btn btn-danger btn-sm ml-2 m-1"
-                  onClick={() => removeItem(item.id)}
-                >
-                  Excluir
-                </button>
-                <button
-                  className="btn btn-secondary btn-sm ml-2 m-1 "
-                  onClick={() => toggleVisibility(item.id, item.isVisible)}
-                >
-                  Alternar Visibilidade
-                </button>
-              </div>
+               {editedItem.id === item.id ? (
+                  <div>
+                    <div className="mb-3">
+                      <label htmlFor="name" className="form-label">
+                        Nome
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        name="name"
+                        value={editedItem.name}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+
+                    <div className="mb-3">
+                      <label htmlFor="description" className="form-label">
+                        Descrição
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="description"
+                        name="description"
+                        value={editedItem.description}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+
+                    <div className="mb-3">
+                      <label htmlFor="price" className="form-label">
+                        Preço
+                      </label>
+                      <input
+                        type="number"
+                        className="form-control"
+                        id="price"
+                        name="price"
+                        value={editedItem.price}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+
+                    <button
+                      className="btn btn-primary"
+                      onClick={handleSaveClick}
+                    >
+                      Salvar
+                    </button>
+                </div>
+              ) : (
+                // Modo de visualização
+                <div>
+                  <span>
+                    <strong className="fs-5" style={{ fontWeight: "bold" }}>
+                      {item.name}
+                    </strong>
+                  </span>
+                  <br />
+                  <span>
+                    <strong>Preço: </strong>R${item.price}
+                  </span>
+                  <br />
+                  <span>
+                    <strong>Ingredientes: </strong>: {item.description}
+                  </span>
+                  {item.isVisible ? " (Visível)" : " (Oculto)"}
+                  <div className="d-flex justify-content-around">
+                    <button
+                      className="btn btn-danger btn-sm ml-2 m-1"
+                      onClick={() => removeItem(item.id)}
+                    >
+                      Excluir
+                    </button>
+                    <button
+                      className="btn btn-secondary btn-sm ml-2 m-1 "
+                      onClick={() => toggleVisibility(item.id, item.isVisible)}
+                    >
+                      Alternar Visibilidade
+                    </button>
+                    <button
+                      className="btn btn-primary btn-sm ml-2 m-1"
+                      onClick={() => handleEditClick(item)}
+                    >
+                      Editar
+                    </button>
+                  </div>
+                </div>
+              )}
             </li>
           ))}
       </ul>
